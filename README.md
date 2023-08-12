f5e27e430f37d0e74ab1d31782931f127053b845
Adrien Combes <me@adriencombes.com>
2023-08-12 02:26:41 +0200

    think about how many cats there is in the world. Well, the best one, from all of them, is legally mine

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 
-- un fichier de config ou on rentre le texte qu'on veut faire apparaitre
-(accessible via une commande)
-- automatise un push tous les jours où il doit activer un pixel
-- log si le pixel a été activé ou non
-- si le fichier de config a changé il reinitialise au prochaine lundi
-- te previent si aujourd'hui tu peux faire des push ou non
-- affiche un aperçu du message qu'il va generer et où il en est
+
+---
+
+## Troubleshoot :
+
+Does this make me a hacker ?<br>
+*Yes, totally. The matrix is yours now. Go get it. :3*
+
+I had a repository named "dont_name_me_like_that" and your f\*\*\*ing script deleted it.<br>
+*Dude, did you really name a repository "dont_name_me_like_that" ? Can't you listen to it ? :(*
+
+I used to enjoy this git functionnality, and I think you kinda ruined it.<br>
+*Shut up, nerd. You're just a dweeb. Get a life man, it's embarassing. Seriously do something, get laid I don't know.*
+
+How long to boil eggs ?<br>
+*Easy : Dippy 3min - Runny yolks 6min - Hard 9min. :)*
+
+Your code is pretty shitty man. Very poor arguments use, and there's not even the complete alphabet.<br>
+*Bro, you got consider this is mainly the work of a pretty high version of me. I mean, is it the only thing that tickles you here ?*
+
+Hey kids ! Do you like violence ?<br>
+*You're too late Marshall sorry. The real slim shady call was an hour ago.*
+
+Don't you think you overload git's servers for dumb things ?<br>
+*Probably. Sorry :/*
+
+Was this dialog thing really wrote by two different persons ?<br>
+*Depends if you hear two different voices in your head or only one. ^^*
+
+How can I delete all this dots and come back to before ?<br>
+*Just delete the repo "dont_name_me_like_that". It will make the associated commits disappear.*
+
+Where can we insult you ?<br>
+*The best way to reach me is through Paypal. Plus, you'll enter a lotery that gives you a chance to win a golden ticket. A very precious ticket that gives you the right to kiss my a\*\*. Anytime, anywere you want, but only once. Otherwise twitter.*
+
+Do you prefer github or gitlab ?<br>
+*Honestly ? Never made the difference. For me it's just the UI changing.*
+
+How to defeat Malenia ?<br>
+*Multiplayer. Frostbite and Hemorrhage. But mostly multiplayer.*
+
+This script doesn't work.<br>
+*Have you tried turning it off and on again?*
--- main.py
+++ main.py
@@ -1,11 +1,118 @@
+##### IMPORTS #####
 
+from datetime import datetime, timedelta
+import numpy as np
 import os
+import pandas as pd
+import pytz
+import plotly.express as px
+import random
 import shutil
 import sys
+from tqdm import tqdm
 
-os.system('gh repo delete --confirm dont_name_me_like_that')
-shutil.copytree(os.path.join('..','amazing_graph'),os.path.join('..','dont_name_me_like_that'))
-os.chdir(os.path.join('..','dont_name_me_like_that'))
-shutil.rmtree('.git')
-os.system('git init -q -b main')
-os.system(f'python message.py "{sys.argv[1]}"')
+from alphabet import alphabet
+
+##### VARIABLES #####
+
+text = sys.argv[1]
+publish = True
+
+##### FUNCTIONS #####
+
+def text_encoder(text):
+    'the most well written docstring'
+    text_encoded = []
+    for t in text:
+        if t == ' ': t_encoded = 'space'
+        elif t == '&': t_encoded = 'heart'
+        else:
+            t_encoded = 'upp_' if t.isupper() else 'low_'
+            t_encoded += t.upper()
+        text_encoded.append(t_encoded)
+        text_encoded.append('space')
+
+    for i, l in enumerate(text_encoded[:-1]):
+        if not i: message = alphabet[l]
+        else: message = np.concatenate([message,alphabet[l]],axis=1)
+
+    if message.shape[1] < 53:
+        rows_to_fill = 53 - message.shape[1]
+        message_filled = np.concatenate((message,np.zeros((7,rows_to_fill),dtype='int')),axis=1)
+    else :
+        message_filled = message[:,:53]
+
+    return message_filled
+
+
+def print_message(array):
+    'the second most well written docstring'
+
+    print('\nThanks for using amazing graph. We love you.')
+    print('Here is a preview of what you should see in git :\n')
+    message_string = ''
+    for index, x in np.ndenumerate(message_filled):
+        if x == 1: message_string += '██'
+        else : message_string += '__'
+        if index[1] == 52:
+            message_string += '\n'
+    print(message_string)
+    pass
+
+
+##### PROCESS #####
+
+message_filled = text_encoder(text)
+message_flat = list(message_filled.reshape(1,371,order='F')[0])
+print_message(message_filled)
+
+publish = True
+
+if publish:
+
+    os.system('gh repo delete --confirm dont_name_me_like_that')
+    shutil.copytree(os.path.join('..','amazing_graph'),os.path.join('..','dont_name_me_like_that'))
+    os.chdir(os.path.join('..','dont_name_me_like_that'))
+    shutil.rmtree('.git')
+    os.system('git init -q -b main')
+
+    all_dates = []
+    active_dates = []
+    deltas = []
+
+    git_delta = int(datetime.now().strftime("%w"))
+
+    for d,p in enumerate(message_flat):
+        delta = int(371-d-(7-git_delta))
+        deltas.append(delta)
+        day = datetime.now(pytz.timezone('Etc/GMT+0'))-timedelta(days=delta)
+        all_dates.append(day)
+        if p == 1:
+            active_dates.append(day)
+
+    days = []
+    for date in all_dates:
+        days.append(date.strftime('%Y.%m.%d'))
+
+    # commits = message_filled*(np.random.rand(7,53)*3+11).astype('int')
+    commits = (message_filled*15).astype('int')
+
+    print('\nMaking commits : ')
+    for n in tqdm(range(len(message_flat))):
+        commits = list(commits.reshape(1,371,order='F'))[0]
+        if deltas[n] > 0:
+            counter = 0
+            while counter < commits[n]:
+                with open('log.txt', 'a') as file:
+                    log = f'\n{str(deltas[n])} days ago - '
+                    log += f'date is {days[n]} - '
+                    log += f'commit {counter+1} / {commits[n]}'
+                    file.write(log)
+                os.system('git add .')
+                os.system(f'git commit -q --date="{days[n]}" -m "{log}"')
+                counter += 1
+
+    print('') # Wassup little print ! Still breaking lines ?
+    os.system('gh repo create --private --source=. --remote=upstream --push')
+    print('\nYou message will appear in a few minutes. Thanks for waiting <3\n')
+    shutil.rmtree(os.path.join('..','dont_name_me_like_that'))
